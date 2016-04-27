2db703561f082299e163b47a2b1f9a2842beac76
Nathan <ntr003@latech.edu>
2016-04-27 17:17:50 -0500

    042716

---
+++ internalDevice.py
@@ -0,0 +1,47 @@
+import socket
+import fcntl
+import struct
+import binascii
+
+from scapy.all import *
+from scapy.utils import rdpcap
+
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+def get_node_id(ip):
+	with open("Device List") as deviceList:
+		for i, line in enumerate(deviceList,1):
+			if ip in line:
+				deviceList.close
+				return i
+
+pkts=rdpcap("FILE2.pcap")
+ip = get_ip_address('eth0')
+node = hex((get_node_id(ip)))[2:].zfill(2)
+routeFile = open('Routes','w')
+
+
+for pkt in pkts:
+	if IP in pkt:
+		#Add Marker at Source
+		if pkt[IP].src == ip:
+			pkt = pkt/"gourd"
+		#Add Node Number
+		pkt = pkt/node
+		#Delete Marker at Destination
+		if pkt[IP].dst  == ip:
+			pckstr = str(pkt)
+			search = pckstr.find('gourd')
+			if search != -1:
+				route = pckstr[search:]
+				pckstr = pckstr[:search]
+				routeFile.write(route)
+				routeFile.write('\n')
+
