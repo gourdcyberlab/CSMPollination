fc7b08a9be84b6401943d10218ce4c9e83a71fea
Nathan <ntr003@latech.edu>
2016-09-14 08:37:50 -0500

    Plus Comments

--- internalDevice.py
+++ internalDevice.py
@@ -8,7 +8,7 @@ from scapy.utils import rdpcap
 from scapy.layers.inet import IP
 
 
-def get_ip_address(ifname):
+def get_ip_address(ifname):						#Get IP Address
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(
         s.fileno(),
@@ -16,36 +16,36 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 
-def get_node_id(ip):
+def get_node_id(ip):							#Gets the NodeID of this node from the list of NodeIDs
 	with open("Device List") as deviceList:
 		for i, line in enumerate(deviceList,1):
 			if ip in line:
 				deviceList.close
 				return i
 
-pkts=rdpcap("FILE2.pcap")
-ip = get_ip_address('eth0')
-node = hex((get_node_id(ip)))[2:].zfill(2)
-routeFile = open('Routes','w')
+pkts=rdpcap("FILE2.pcap")						#Seeks packets in a PCAP file. This will need to be replaced to be able to do this 'live'
+ip = get_ip_address('eth0')						#Gets IP of network device executing the code
+node = hex((get_node_id(ip)))[2:].zfill(2)				#Gets the NodeID
+routeFile = open('Routes','w')						#Creates a file to record all the routes of packets exiting the network or ending at this node
 
 
 for pkt in pkts:
 	if IP in pkt:
-		del pkt.chksum
+		del pkt.chksum						#Deletes the packet's checksum. Scapy recalculates the checksum later
 		#Add Marker at Source
-		if pkt[IP].src == ip:
-			pkt = pkt/"gourd"
+		if pkt[IP].src == ip:					#If the packet originated at this node
+			pkt = pkt/"gourd"				#Add the marker
+			pkt = pkt/node					#Also add the NodeID
 		#Add Node Number
-		pkt = pkt/node
+		pkt = pkt/node						#If the packet is just passing through this node, add the NodeID
 		#Delete Marker at Destination
-		if pkt[IP].dst  == ip:
-			pckstr = str(pkt)
-			search = pckstr.find('gourd')
+		if pkt[IP].dst  == ip:					#If the packet's destination is this node
+			pckstr = str(pkt)				#Create a edit-able string from the packet
+			search = pckstr.find('gourd')			#Find the marker
 			if search != -1:
-				route = pckstr[search:]
-				pckstr = pckstr[:search]
-				#Record the route of the packet to file
-				routeFile.write(route)
+				route = pckstr[search:]			#The route will be everything after the marker
+				pckstr = pckstr[:search]		#We rewrite the packet as everything before the marker
+				routeFile.write(route)			#Record the route of the packet to file
 				routeFile.write('\n')
-			pkt = pkt.__class__(pckstr)
-	#send(pkt)
+			pkt = pkt.__class__(pckstr)			#Rewrites the packet as the string we were editing
+	#send(pkt)							#Sends the packet
--- networkEdge.py
+++ networkEdge.py
@@ -7,7 +7,7 @@ from scapy.all import *
 from scapy.utils import rdpcap
 from scapy.layers.inet import IP
 
-def get_ip_address(ifname):
+def get_ip_address(ifname):						#Get IP Address
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(
         s.fileno(),
@@ -15,38 +15,72 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 
-def inNetwork(ip):
-	ipfile = file('Device List')
+def inNetwork(ip):							#Test to see if IP Address is in the network
+	ipfile = file('Device List')					#Device List is the file that contains all IPs in network
 	for line in ipfile:
 		if ip in line:
 			return True
 	return False
 
-pkts=rdpcap("FILE2.pcap")
-ip = get_ip_address('eth0')
-routeFile = open('Routes','w')
+def get_node_id(ip):							#Gets the NodeID of this node from the list of NodeIDs
+	with open("Device List") as deviceList:
+		for i, line in enumerate(deviceList,1):
+			if ip in line:
+				deviceList.close
+				return i
 
-for pkt in pkts:
+pkts=rdpcap("FILE2.pcap")						#Seeks packets in a PCAP file. This will need to be replaced to be able to do this 'live'
+ip = get_ip_address('eth0')						#Gets IP of network device executing the code
+node = hex((get_node_id(ip)))[2:].zfill(2)				#Gets the NodeID
+routeFile = open('Routes','w')						#Creates a file to record all the routes of packets exiting the network or ending at this node
+
+for pkt in pkts:							#Here is where the packets are actually altered
 	if IP in pkt:
-		del pkt.chksum
+		del pkt.chksum						#Deletes the packet's checksum. Scapy recalculates the checksum later
 		
-		#Delete Marker on Outgoing
-		if inNetwork(pkt[IP].dst):
-			pkt = pkt/"EDGE"
-			pckstr = str(pkt)
-			search = pckstr.find('gourd')
-			if search != -1:
-				route = pckstr[search:]
-				pckstr = pckstr[:search]
-				#Record the route of the packet to file
-				routeFile.write(route)
+		pckstr = str(pkt)					#Creates a string of the contents of the packet. We can edit this string and later save is back as the packet
+		search = pckstr.find('gourd')				#Find our marker in the packet
+
+		if search != -1:					#If the marker is already in the packet. This means the packet is not entering the network at this node
+			
+			if inNetwork(pkt[IP].dst):			#Checks to see if destination IP is inside or outside the network. If the destination is within the network, it doesn't exit here.
+				pckstr =+ node				#Adds the NodeID to the end of the packet string.
+			
+			else:						#The packet exits the network here. We need to save our route info and remove it from the packet
+				route = pckstr[search:]			#The route will be everything after the marker
+				pckstr = pckstr[:search]		#We rewrite the packet as everything before the marker
+				routeFile.write(route)			#Record the route of the packet to file
 				routeFile.write('\n')
-			pkt = pkt.__class__(pckstr)
+			pkt = pkt.__class__(pckstr)			#Rewrites the packet as the string we were editing
+
+		else:							#If the marker isn't already present, we assume it enters the network at this node
+				pkt = pkt/"gourd"			#Adds the marker to the packet
+				pkt = pkt/"EDGE"			#Adds the string "EDGE" to the packet
+		#send(pkt)						#Sends the packet			
+	
+
+'''
+		#Delete Marker on Outgoing
+		if inNetwork(pkt[IP].dst):				#Checks to see if destination IP is inside or outside the network
+			pkt = pkt/node					#Adds the NodeID to the end of the packet
+		else:							#If the destination address is outside the network
+			pkt = pkt/"EDGE"				#Add the string "EDGE" to the end of the packet
+			pckstr = str(pkt)				#Creates a string of the contents of the packet. We can edit this string and later save is back as the packet
+			search = pckstr.find('gourd')			#Find our marker in the packet
+			if search != -1:				
+				route = pckstr[search:]			#The route will be everything after the marker
+				pckstr = pckstr[:search]		#We rewrite the packet as everything before the marker
+				routeFile.write(route)			#Record the route of the packet to file
+				routeFile.write('\n')
+			pkt = pkt.__class__(pckstr)			#Rewrites the packet as the string we were editing
 		
 		#Create Marker on Incoming
-		if inNetwork(pkt[IP].src):
-			pkt = pkt/"gourd"
-			pkt = pkt/"EDGE"
+		if inNetwork(pkt[IP].src):'
+			
+		else:							#If the source address is from outside the network
+			pkt = pkt/"gourd"				#Adds the marker to the packet
+			pkt = pkt/"EDGE"				#Adds the string "EDGE" to the packet
 		
-		#send(pkt)
+		#send(pkt)						#Sends the packet
+'''
 
